070a39e2aa07f1140a7b626d07db186a14d2c076
RyanOrsinger <ryan.orsinger@gmail.com>
2019-01-18 11:36:25 -0600

    add more chapter4 notes

--- chapter4.py
+++ chapter4.py
@@ -23,3 +23,38 @@ data.ndim #shows the n number of dimensions
 np.zeros(100) # produces an array of 100 zeros
 np.ones(123) # produces an array of 123 1s
 
+x = np.array([1, 2, 3, 4, 5])
+
+# numpy is all about vectorization
+print(x*5)  
+
+
+# .reshape reshapes the data
+arr = np.arange(15).reshape((3, 5))
+
+arr = arr.reshape(5, 3)
+
+arr = arr.reshape(1, 15)
+
+# .T transposes
+arr.T 
+
+# Using transpose with dot product
+arr = np.random.randn(6, 3)
+np.dot(arr.dot(arr.T, arr))
+
+# unary ufunctions
+np.square(arr)
+np.log(arr)
+
+# vectorization
+
+arr = np.random.randn(4, 4)
+arr > 0
+np.where(arr > 0, 1, 0)
+
+# Statistical methods
+
+arr.mean()
+np.mean(arr)
+arr.sum()
